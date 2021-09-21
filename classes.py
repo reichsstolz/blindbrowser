5d751995106b6c5c1e32d560d36aa39523615f23
reichsstolz <styrkoffpost@gmail.com>
2021-09-21 22:52:28 +0300

    Start

---
+++ classes.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+        pass    
