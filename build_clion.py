ee49993491bf5c90a1053b22588685c49949342a
reichsstolz <styrkoffpost@gmail.com>
2021-10-19 01:09:34 +0300

    fix build_clion.py

--- build_clion.py
+++ build_clion.py
@@ -1,10 +1,7 @@
-import os, py_compile
+import py_compile
 
-
-os.chdir("..")
-os.chdir("conan")
-os.system("conan install .")
-os.chdir("..")
+print("Compiling python files...")
 py_compile.compile("python_files/MainClasses.py", "cmake-build-debug/bin/MainClasses.pyc")
 py_compile.compile("python_files/MinorClasses.py", "cmake-build-debug/bin/MinorClasses.pyc")
 py_compile.compile("python_files/functions.py", "cmake-build-debug/bin/functions.pyc")
+print("Compiling complete!")
